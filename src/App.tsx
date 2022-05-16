6df2a4aaa4f761aea8b0c45c590b89b28ac988d3
akshay moradiya <akshay.trinityinfosystem@gmail.com>
2022-05-16 14:53:06 +0530

    data context and authentication sytem and api module

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,47 @@
+import { createContext, useReducer } from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { SET_REFRESH_TOKEN } from "./actions/auth";
+import defaultConfigurations from "./config/auth";
 import Layout from './Layout/Default/Layout';
 import Contact from './pages/Contact/Contact';
+import rootReducer, { rootState, initialState as rootStateTypes } from "./reducer";
+export interface context { 
+  state: rootStateTypes,
+  dispatch: Function
+}
+
+export const GlobalContext = createContext<context>({
+  state: rootState,
+  dispatch: () => {}
+})
 
 function App() {
+  
+  const [state, dispatch] = useReducer(rootReducer, rootState);
+
+  if(!state?.auth?.refreshToken){
+    dispatch({
+      type: SET_REFRESH_TOKEN,
+      payload: defaultConfigurations.credentials.refreshToken
+    })
+  }
+
+  const contextValue: context = {
+    state,
+    dispatch
+  }
+
   return (
-    <Router>
-      <Routes>
-        <Route path='/' element={<Layout/>}>
-          <Route path='contact' element={<Contact />} >
+    <GlobalContext.Provider value={contextValue}>
+      <Router>
+        <Routes>
+          <Route path='/' element={<Layout/>}>
+            <Route path='contact' element={<Contact />} >
+            </Route>
           </Route>
-        </Route>
-      </Routes>
-    </Router>
+        </Routes>
+      </Router>
+    </GlobalContext.Provider>
   );
 }
 
--- src/Layout/Default/Layout.tsx
+++ src/Layout/Default/Layout.tsx
@@ -1,18 +1,62 @@
-import { useEffect } from 'react';
+import { Box, Typography } from '@mui/material';
+import { useContext, useEffect, useState } from 'react';
 import { Outlet, useNavigate, useLocation } from 'react-router';
-import {  } from "@material-ui/core/styles";
+import { SET_AUTH } from '../../actions/auth';
+import agent from '../../agent';
+import { context, GlobalContext } from '../../App';
+import defaultConfigurations from '../../config/auth';
 import StyledContainer from "./styles";
 
 const Layout = () => {
   const navigate = useNavigate();
   const location =  useLocation();
-
+  const { state : { auth }, dispatch } = useContext<context>(GlobalContext);
+  const [isAppReady, setIsAppReady] = useState(true);
+  const { credentials, token } = defaultConfigurations
   //custom redirect as of now
   useEffect(() => {
     if(location.pathname === "/"){
       navigate('/contact')
     }
   }, [location, navigate]);
+
+  const autoLogin = () => 
+    agent.Auth.getAccessToken(credentials.teamId, auth.refreshToken || credentials.refreshToken).then(data => {
+      localStorage.setItem(token, data?.access_token)
+      dispatch({
+        type: SET_AUTH,
+        payload: data?.access_token
+      })
+    })
+  
+  
+  if(!auth.accessToken){
+    let accessToken : string | null = localStorage.getItem("access_token")
+    if(!accessToken){      
+      autoLogin()
+    }else {
+      dispatch({
+        type: SET_AUTH,
+        payload: accessToken
+      })
+    }
+  }  
+
+  if(!isAppReady){
+    return (
+      <StyledContainer>
+        <Box 
+          width="100%"
+          height="100vh" 
+          display="flex"
+          justifyContent="center"
+          alignItems="center"
+        >
+          <Typography variant='h3' fontWeight={600} >Sorry, Service is currently unavailable!!</Typography>
+        </Box>
+      </StyledContainer>
+    )
+  }
 
   return (
     <StyledContainer  disableGutters>
